0dc0345c03c3611b44d891f3cebebe8d8f550ab7
shimat <schimatk@gmail.com>
2023-05-08 10:31:09 +0900

    Size

--- cs/OpenCvSharp5/Modules/core/Struct/Size.cs
+++ cs/OpenCvSharp5/Modules/core/Struct/Size.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 namespace OpenCvSharp5;
 
@@ -8,80 +7,46 @@ namespace OpenCvSharp5;
 /// </summary>
 [Serializable]
 [StructLayout(LayoutKind.Sequential)]
-[SuppressMessage("Design", "CA1051: Do not declare visible instance fields")]
-public struct Size : IEquatable<Size>
+public record struct Size(int Width, int Height)
 {
     /// <summary>
-    /// 
     /// </summary>
-    public int Width;
+    public int Width = Width;
 
     /// <summary>
-    /// 
     /// </summary>
-    public int Height;
+    public int Height = Height;
+}
+
+/// <summary>
+/// 
+/// </summary>
+[Serializable]
+[StructLayout(LayoutKind.Sequential)]
+// ReSharper disable once InconsistentNaming
+public record struct Size2f(float Width, float Height)
+{
+    /// <summary>
+    /// </summary>
+    public float Width = Width;
 
     /// <summary>
-    /// Constructor
     /// </summary>
-    /// <param name="width"></param>
-    /// <param name="height"></param>
-    public Size(int width, int height)
-    {
-        Width = width;
-        Height = height;
-    }
+    public float Height = Height;
+}
+
+/// <summary>
+/// 
+/// </summary>
+[Serializable]
+[StructLayout(LayoutKind.Sequential)]
+public record struct Size2d(double Width, double Height)
+{
+    /// <summary>
+    /// </summary>
+    public double Width = Width;
 
     /// <summary>
-    /// Constructor
     /// </summary>
-    /// <param name="width"></param>
-    /// <param name="height"></param>
-    public Size(double width, double height)
-    {
-        Width = (int)width;
-        Height = (int)height;
-    }
-
-    /// <summary>
-    /// Zero size
-    /// </summary>
-    public static readonly Size Zero;
-
-    #region Operators
-
-    /// <summary>
-    /// Compares two CvPoint objects. The result specifies whether the members of each object are equal.
-    /// </summary>
-    /// <param name="lhs">A Point to compare.</param>
-    /// <param name="rhs">A Point to compare.</param>
-    /// <returns>This operator returns true if the members of left and right are equal; otherwise, false.</returns>
-    public static bool operator ==(Size lhs, Size rhs) => lhs.Equals(rhs);
-
-    /// <summary>
-    /// Compares two CvPoint objects. The result specifies whether the members of each object are unequal.
-    /// </summary>
-    /// <param name="lhs">A Point to compare.</param>
-    /// <param name="rhs">A Point to compare.</param>
-    /// <returns>This operator returns true if the members of left and right are unequal; otherwise, false.</returns>
-    public static bool operator !=(Size lhs, Size rhs) => !lhs.Equals(rhs);
-
-    #endregion
-
-    #region Override
-        
-    /// <inheritdoc />
-    public readonly bool Equals(Size other) => Width == other.Width && Height == other.Height;
-
-    /// <inheritdoc />
-    public readonly override bool Equals(object? obj) => obj is Size other && Equals(other);
-
-    /// <inheritdoc />
-    public readonly override int GetHashCode() => HashCode.Combine(Width, Height);
-
-    /// <inheritdoc />
-    public readonly override string ToString() => $"(width:{Width} height:{Height})";
-
-    #endregion
-
+    public double Height = Height;
 }
